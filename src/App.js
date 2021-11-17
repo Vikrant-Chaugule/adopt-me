7a485d4324c0c758517b13fee780002a8ed71f32
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-11-17 09:04:26 +0530

    moved App component to separate file and created Pet component

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = ({name, animal, breed}) => {
+    return React.createElement(
+        "div", {}, [
+            React.createElement("h2", {}, name),
+            React.createElement("h3", {}, animal),
+            React.createElement("h3", {}, breed)
+        ]
+    );
+}
+
+const App = () => {
+    return React.createElement(
+        "div", 
+        {},
+        [
+        React.createElement(
+            "h1",
+            {},
+            "Adopt Me!" 
+        ),
+        React.createElement(Pet, {name: "Molly", animal: "Dog", breed: "Havanese"}),
+        React.createElement(Pet, {name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
+        React.createElement(Pet, {name: "Scotty", animal: "Dog", breed: "Wheaten Terrier"}),
+    ]
+    );
+};
+// tell React, where to render this component
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
