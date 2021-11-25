bf199985e6dc34d5d65d1abf0810f066f7f0282a
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-11-25 18:53:15 +0530

    converted App and Pet components to JSX components

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,22 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
+// const Pet = ({ name, animal, breed }) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, name),
+//     React.createElement("h3", {}, animal),
+//     React.createElement("h3", {}, breed),
+//   ]);
+// };
+
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
