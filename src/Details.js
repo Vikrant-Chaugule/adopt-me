77625624f0b540252aa28efe5540d3f457aee464
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-11-28 20:42:29 +0530

    updated Details component to show all the info

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,45 @@
-const Details = () => {
-  return (
-    <div>
-      <h2>Hi LOL</h2>
-    </div>
-  );
-};
+import { Component } from "react";
+import { withRouter } from "react-router";
 
-export default Details;
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          isLoading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// withRouter is a higher order component that wraps the Details component
+export default withRouter(Details);
