a0a6c1f4bf0a555335a27f6f4673db4499bb267e
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-11-28 15:02:05 +0530

    added routes for details page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id" component={Details} exact />
+        </Switch>
+        <Route path="/" component={SearchParams} exact />
+      </Router>
     </div>
   );
 };
