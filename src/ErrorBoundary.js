a8d0179ff2018fac8c490bd4f3e45314515fd721
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-12-03 20:29:03 +0530

    redirect user to home page in 5 seconds if there is an error in Pet Details

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,9 +1,10 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = {
     hasError: false,
+    redirect: false,
   };
 
   static getDerivedStateFromError() {
@@ -13,14 +14,18 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     // log error to an error logging service like Azure Monitor, Sentry, etc.
     console.log("Error boundary caught an error ", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing is not available at the moment.
-          <Link to="/">Click here </Link>to go back to the home page
+          <Link to="/">Click here </Link>to go back to the home page or wait 5
+          seconds.
         </h2>
       );
     }
