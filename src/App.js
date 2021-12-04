b863cfeea555908e877b8380b3163596f8cf2aae
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2021-12-04 19:45:51 +0530

    created a ThemeContext for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("black");
+
   return (
-    <div>
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id" component={Details} exact />
-        </Switch>
-        <Route path="/" component={SearchParams} exact />
-      </Router>
-    </div>
+    // anything wrapped inside context provider will have access to the value e.g. theme
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id" component={Details} exact />
+          </Switch>
+          <Route path="/" component={SearchParams} exact />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
